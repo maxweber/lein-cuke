59c8f32a59546583f2fc7a9f8d18cfe00c8f66cb
Maximilian Weber <weber.maximilian@googlemail.com>
2011-06-08 12:50:25 +0200

    cuke-gems has been changed so that it also fetchs the cuke4duke gem to get the cuke4duke/lib/cucumber/clj_support/clj_language.rb which is need for the Cucumber Clojure support.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-cuke "0.0.2"
+(defproject lein-cuke "0.0.3"
   :description "Cucumber runner for leiningen"
   :repositories {"cukes" "http://cukes.info/maven"
                  "clojars" "http://clojars.org/repo"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [cuke4duke "0.4.3"
+                 [cuke4duke "0.4.4"
                   :exclusions [org.clojure/clojure
                                org.clojure/clojure-contrib]]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
